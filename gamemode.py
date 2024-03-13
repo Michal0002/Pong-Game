c07a83bd89b6adab6ee6fbfe4c5e884d65b34747
Michał Kasperek <michal.kasperek2000@wp.pl>
2024-03-13 13:34:23 +0100

    Score display, reset game, colisions, sound effects for the menu

--- gamemode.py
+++ gamemode.py
@@ -8,36 +8,44 @@ width, height = 800, 600
 #sounds
 collision_sound = pygame.mixer.Sound('sounds/ball_sound.wav')
 round_over_sound = pygame.mixer.Sound('sounds/round_over.wav')
-computer_size = (width // 10, 20)
-computer_position = [width // 2 - computer_size[0] // 2, 0]
-player_size = (width// 10, 20)
 
+def display_message(screen, message):
+    font = pygame.font.SysFont("Comic Sans MS", 50)
+    text = font.render(message, True, (255, 255, 255))
+    text_rect = text.get_rect(center=(width // 2, height // 2))
+    screen.blit(text, text_rect)
+    pygame.display.update()
 
 def singleplayer():
+    global player_position, computer_position, ball_position, ball_direction, start_time, player_won, player_score, computer_score, player_size, computer_size, computer_speed
 
     #player pallete
+    player_size = (width // 10, 20)
     player_speed = 8
     player_position = [width // 2 - player_size[0] // 2, height - 2 * player_size[1]]
 
-    #pc pallete
-    computer_speed = 5  
+    #pc palette
+    computer_size = (width // 10, 20)
+    computer_position = [width // 2 - computer_size[0] // 2, 0]
+    computer_speed = 5
     computer_reaction_time = 500 * 30
 
     screen = pygame.display.set_mode((width, height))
+
     ball_size = 20
     ball_speed = 5
-    ball_direction = [random.choice([-1,1]), random.choice([-1,1])]
-    ball_position = [width // 2, height //2] 
+    ball_direction = [random.choice([-1, 1]), random.choice([-1, 1])]
+    ball_position = [width // 2, height // 2]
 
-    #timer and delay
+    #timer
     elapsed_time = 0
     player_won = False
     start_time = pygame.time.get_ticks()
     timer = pygame.time.Clock()
     game_over = False
-    
+
     player_score = 0
-    computer_score = 0 
+    computer_score = 0
 
     while not game_over:
         for event in pygame.event.get():
@@ -48,14 +56,35 @@ def singleplayer():
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-        #ball movement with colission with site edges
+
+        #ball movement with collision with site edges
         ball_position[0] += ball_speed * ball_direction[0]
         ball_position[1] += ball_speed * ball_direction[1]
 
         if ball_position[0] <= 0 or ball_position[0] >= width - ball_size:
             ball_direction[0] *= -1
-        elif ball_position[1] <= 0 or ball_position[1] >= height - ball_size:
-            ball_direction[1] *= -1
+        elif ball_position[1] <= 0:
+            player_score += 1
+            if player_score >= 3:
+                round_over_sound.play()
+                display_message(screen, "You won the round!")
+                pygame.time.delay(3000)
+                return True
+            else:
+                display_message(screen, "You scored!")
+                pygame.time.delay(1000)
+                reset_position()
+        elif ball_position[1] >= height - ball_size:
+            computer_score += 1
+            if computer_score >= 3:
+                round_over_sound.play()
+                display_message(screen, "Computer won the round!")
+                pygame.time.delay(3000)
+                return True
+            else:
+                display_message(screen, "Computer scored!")
+                pygame.time.delay(1000)
+                reset_position()
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
@@ -70,56 +99,48 @@ def singleplayer():
         if not player_won and elapsed_time >= computer_reaction_time:
             computer_speed = 1
             player_won = True
-
+            print("Po 15s: " + str(computer_speed))        
+            
         if computer_position[0] + computer_size[0] // 2 < ball_position[0]:
             computer_position[0] += computer_speed
-        elif computer_position[0] + computer_size[0] // 2 > ball_position[0]:               
+        elif computer_position[0] + computer_size[0] // 2 > ball_position[0]:
             computer_position[0] -= computer_speed
 
-        #collision with palletes
-        if (player_position[1] <= ball_position[1] + ball_size and player_position[1] + player_size[1] >= ball_position[1] and player_position[0] <= ball_position[0] <= player_position[0] +player_size[0]):
+        #collision with palettes
+        if (player_position[1] <= ball_position[1] + ball_size and player_position[1] + player_size[1] >= ball_position[1] and player_position[0] <= ball_position[0] <= player_position[0] + player_size[0]):
             ball_direction[1] *= -1
             collision_sound.play()
 
-        elif (computer_position[1] <= ball_position[1] + ball_size and computer_position[1] + computer_size[1] >= ball_position[1] and computer_position[0] <= ball_position[0] <= computer_position[0] +computer_size[0]):
+        elif (computer_position[1] <= ball_position[1] + ball_size and computer_position[1] + computer_size[1] >= ball_position[1] and computer_position[0] <= ball_position[0] <= computer_position[0] + computer_size[0]):
             ball_direction[1] *= -1
             collision_sound.play()
-
-        #bottom edge collisionn
-        if ball_position[1] <= 0:
-            player_score += 1
-            if player_score >= 3:
-                game_over = True
-                round_over_sound.play()
-        elif ball_position[1] >= height - ball_size:
-            computer_score += 1
-            if computer_score >= 3:
-                game_over = True
-                round_over_sound.play()
-
 
         screen.fill("black")
         elapsed_seconds = elapsed_time / 1000
         font = pygame.font.SysFont("Comic Sans MS", 12)
         timer_text = font.render("Time: {:.2f}s".format(elapsed_seconds), True, (99, 255, 11))
-        player_score_text = font.render(f"Gracz: {player_score}", True, (255, 255, 255))
-        computer_score_text = font.render(f"Komputer: {computer_score}", True, (255, 255, 255))
-        player_score_text_rect = player_score_text.get_rect(midbottom=(width // 4, height - 10))
-        computer_score_text_rect = computer_score_text.get_rect(midbottom=(width * 3 // 4, height - 10))
-
-        screen.blit(player_score_text, player_score_text_rect)
-        screen.blit(computer_score_text, computer_score_text_rect)
+        
+        score_text = font.render(f"You: {player_score} - Computer: {computer_score}", True, (255, 255, 255))
+        score_text_rect = score_text.get_rect(midbottom=(width // 2, height))
+        screen.blit(score_text, score_text_rect)
         screen.blit(timer_text, (width - timer_text.get_width(), height - timer_text.get_height()))
         pygame.draw.ellipse(screen, "white", (ball_position[0], ball_position[1], ball_size, ball_size))
         pygame.draw.rect(screen, "lightblue", (player_position[0], player_position[1], player_size[0], player_size[1]))
         pygame.draw.rect(screen, "white", (computer_position[0], computer_position[1], computer_size[0], computer_size[1]))
 
         pygame.display.update()
-        timer.tick(60)  
+        timer.tick(60)
 
-def reset_positions():
-    global ball_position, player_position, computer_position, ball_direction
-    ball_position = [width // 2, height // 2]
+def reset_position():
+    pygame.time.delay(3000)
+    global player_position, computer_position, ball_position, ball_direction, start_time, player_won, computer_speed, computer_reaction_time,elapsed_time
     player_position = [width // 2 - player_size[0] // 2, height - 2 * player_size[1]]
     computer_position = [width // 2 - computer_size[0] // 2, 0]
-    ball_direction = [random.choice([-1,1]), random.choice([-1,1])]
+    ball_position = [width // 2, height // 2]
+    ball_direction = [random.choice([-1, 1]), random.choice([-1, 1])]
+    start_time = pygame.time.get_ticks()
+    player_won = False
+    elapsed_time = pygame.time.get_ticks() - start_time
+    computer_speed = 5
+    computer_reaction_time = 500 * 30 
+    print("W po resecie: " + str(computer_speed))
--- main.py
+++ main.py
@@ -4,25 +4,36 @@ import gamemode
 
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
+
 pygame.display.set_caption("Pong Game")
 
 pygame.init()
 
 font = pygame.font.SysFont('Comic Sans MS', 36)
 font_color = (192, 192, 192)
+highlight_color = (255, 255, 0)  # Kolor podświetlenia
+selected_option = 1  # Początkowo pierwsza opcja jest wybrana
 
-def draw_text(text, x, y):
-    text_surface = font.render(text, True, font_color)
+def init_sounds():
+    select_sound = pygame.mixer.Sound('sounds/selection.wav')
+    choose_sound = pygame.mixer.Sound('sounds/after_choose.wav')
+    return select_sound, choose_sound
+
+def draw_text(text, x, y, highlighted=False):
+    color = highlight_color if highlighted else font_color
+    text_surface = font.render(text, True, color)
     text_rect = text_surface.get_rect(center=(x, y))
     screen.blit(text_surface, text_rect)
 
 def main_menu():
+    select_sound, choose_sound = init_sounds()
+    global selected_option
     while True:
         screen.fill('black')
-        draw_text("Pong Game", width //2 , height // 3)
-        draw_text("1. Singleplayer game", width // 2, height // 2)
-        draw_text("2. Multiplayer mode", width // 2, height // 2 + 50)
-        draw_text("3. Exit", width // 2, height // 2 + 100)
+        draw_text("Pong Game", width // 2, height // 3)
+        draw_text("1. Singleplayer game", width // 2, height // 2, selected_option == 1)
+        draw_text("2. Multiplayer mode", width // 2, height // 2 + 50, selected_option == 2)
+        draw_text("3. Exit", width // 2, height // 2 + 100, selected_option == 3)
         pygame.display.flip()
         events = pygame.event.get()
         for event in events:
@@ -37,5 +48,22 @@ def main_menu():
                 elif event.key == pygame.K_3 or event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
+                elif event.key == pygame.K_UP:
+                    select_sound.play()
+                    selected_option = max(1, selected_option - 1)
+                elif event.key == pygame.K_DOWN:
+                    select_sound.play()
+                    selected_option = min(3, selected_option + 1)
+                elif event.key == pygame.K_RETURN:
+                    if selected_option == 1:
+                        choose_sound.play()
+                        gamemode.singleplayer()
+                    elif selected_option == 2:
+                        choose_sound.play()
+                        main_menu()
+                    elif selected_option == 3:
+                        choose_sound.play()
+                        pygame.quit()
+                        sys.exit()
 
 main_menu()
